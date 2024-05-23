dbf8ea24a1c14d11da90ed6de75d99b5e3a27f17
Nathan <nathang.idaho@gmail.com>
2024-05-23 18:37:19 +0200

    Ajoutez des fichiers projet.

---
+++ Menu/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Menu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            tableLayoutPanel1 = new TableLayoutPanel();
+            pictureBox1 = new PictureBox();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            btnContinuer = new Button();
+            tableLayoutPanel3 = new TableLayoutPanel();
+            btnNouvellePartie = new Button();
+            tableLayoutPanel4 = new TableLayoutPanel();
+            btnOptions = new Button();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            tableLayoutPanel2.SuspendLayout();
+            tableLayoutPanel3.SuspendLayout();
+            tableLayoutPanel4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(pictureBox1, 0, 0);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 1);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel3, 0, 2);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel4, 0, 3);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 5;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 12.5F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 12.5F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 12.5F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 12.5F));
+            tableLayoutPanel1.Size = new Size(1182, 628);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(3, 30);
+            pictureBox1.Margin = new Padding(3, 30, 3, 30);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1176, 254);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 3;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 37.5F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 37.5F));
+            tableLayoutPanel2.Controls.Add(btnContinuer, 1, 1);
+            tableLayoutPanel2.Dock = DockStyle.Fill;
+            tableLayoutPanel2.Location = new Point(3, 317);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 3;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 80F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanel2.Size = new Size(1176, 72);
+            tableLayoutPanel2.TabIndex = 1;
+            // 
+            // btnContinuer
+            // 
+            btnContinuer.BackColor = Color.FromArgb(224, 224, 224);
+            btnContinuer.Dock = DockStyle.Fill;
+            btnContinuer.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            btnContinuer.Location = new Point(444, 10);
+            btnContinuer.Name = "btnContinuer";
+            btnContinuer.Size = new Size(288, 51);
+            btnContinuer.TabIndex = 0;
+            btnContinuer.Text = "CONTINUER";
+            btnContinuer.UseVisualStyleBackColor = false;
+            btnContinuer.Click += btnContinuer_Click;
+            btnContinuer.MouseEnter += btn_MouseEnter;
+            btnContinuer.MouseLeave += btn_MouseLeave;
+            // 
+            // tableLayoutPanel3
+            // 
+            tableLayoutPanel3.ColumnCount = 3;
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 37.5F));
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 37.5F));
+            tableLayoutPanel3.Controls.Add(btnNouvellePartie, 1, 1);
+            tableLayoutPanel3.Dock = DockStyle.Fill;
+            tableLayoutPanel3.Location = new Point(3, 395);
+            tableLayoutPanel3.Name = "tableLayoutPanel3";
+            tableLayoutPanel3.RowCount = 3;
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 80F));
+            tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanel3.Size = new Size(1176, 72);
+            tableLayoutPanel3.TabIndex = 2;
+            // 
+            // btnNouvellePartie
+            // 
+            btnNouvellePartie.BackColor = Color.FromArgb(224, 224, 224);
+            btnNouvellePartie.Dock = DockStyle.Fill;
+            btnNouvellePartie.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            btnNouvellePartie.Location = new Point(444, 10);
+            btnNouvellePartie.Name = "btnNouvellePartie";
+            btnNouvellePartie.Size = new Size(288, 51);
+            btnNouvellePartie.TabIndex = 0;
+            btnNouvellePartie.Text = "NOUVELLE PARTIE";
+            btnNouvellePartie.UseVisualStyleBackColor = false;
+            btnNouvellePartie.Click += btnNouvellePartie_Click;
+            btnNouvellePartie.MouseEnter += btn_MouseEnter;
+            btnNouvellePartie.MouseLeave += btn_MouseLeave;
+            // 
+            // tableLayoutPanel4
+            // 
+            tableLayoutPanel4.ColumnCount = 3;
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 37.5F));
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 37.5F));
+            tableLayoutPanel4.Controls.Add(btnOptions, 1, 1);
+            tableLayoutPanel4.Dock = DockStyle.Fill;
+            tableLayoutPanel4.Location = new Point(3, 473);
+            tableLayoutPanel4.Name = "tableLayoutPanel4";
+            tableLayoutPanel4.RowCount = 3;
+            tableLayoutPanel4.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanel4.RowStyles.Add(new RowStyle(SizeType.Percent, 80F));
+            tableLayoutPanel4.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanel4.Size = new Size(1176, 72);
+            tableLayoutPanel4.TabIndex = 3;
+            // 
+            // btnOptions
+            // 
+            btnOptions.BackColor = Color.FromArgb(224, 224, 224);
+            btnOptions.Dock = DockStyle.Fill;
+            btnOptions.Font = new Font("Segoe UI", 10F, FontStyle.Bold);
+            btnOptions.Location = new Point(444, 10);
+            btnOptions.Name = "btnOptions";
+            btnOptions.Size = new Size(288, 51);
+            btnOptions.TabIndex = 0;
+            btnOptions.Text = "OPTIONS";
+            btnOptions.UseVisualStyleBackColor = false;
+            btnOptions.Click += btnOptions_Click;
+            btnOptions.MouseEnter += btn_MouseEnter;
+            btnOptions.MouseLeave += btn_MouseLeave;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(0, 0, 43);
+            ClientSize = new Size(1182, 628);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Form1";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Menu Principal";
+            WindowState = FormWindowState.Maximized;
+            SizeChanged += Form1_SizeChanged;
+            tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel3.ResumeLayout(false);
+            tableLayoutPanel4.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private PictureBox pictureBox1;
+        private TableLayoutPanel tableLayoutPanel2;
+        private TableLayoutPanel tableLayoutPanel3;
+        private Button btnNouvellePartie;
+        private TableLayoutPanel tableLayoutPanel4;
+        private Button btnOptions;
+        private Button btnContinuer;
+    }
+}
---
+++ Menu/Form1.cs
@@ -0,0 +1,46 @@
+namespace Menu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            float fontHeight = btnContinuer.Size.Height / 5;
+
+            btnContinuer.Font = new Font(btnContinuer.Font.FontFamily, fontHeight, FontStyle.Bold);
+            btnNouvellePartie.Font = new Font(btnNouvellePartie.Font.FontFamily, fontHeight, FontStyle.Bold);
+            btnOptions.Font = new Font(btnOptions.Font.FontFamily, fontHeight, FontStyle.Bold);
+        }
+
+        private void btn_MouseEnter(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.BackColor = Color.LightBlue;
+        }
+
+        private void btn_MouseLeave(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.BackColor = Color.FromArgb(224, 224, 224);
+        }
+
+        private void btnContinuer_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Continuer");
+        }
+
+        private void btnNouvellePartie_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Nouvelle Partie");
+        }
+
+        private void btnOptions_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Options");
+        }
+    }
+}
